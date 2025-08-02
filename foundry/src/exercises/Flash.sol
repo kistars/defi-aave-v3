2112d1ae4bdfa9bc39cd7de1d77985fe68d5f118
kistars <sunllen2014@outlook.com>
2025-08-02 22:26:17 +0800

    feat: flash loan exercise

--- foundry/src/exercises/Flash.sol
+++ foundry/src/exercises/Flash.sol
@@ -10,7 +10,10 @@ contract Flash {
     IPool public constant pool = IPool(POOL);
 
     // Task 1 - Initiate flash loan
-    function flash(address token, uint256 amount) public {}
+    function flash(address token, uint256 amount) public {
+        bytes memory data = abi.encode(msg.sender);
+        pool.flashLoanSimple(msg.sender, token, amount, data, 0);
+    }
 
     // Task 2 - Repay flash loan
     function executeOperation(
@@ -21,14 +24,20 @@ contract Flash {
         bytes calldata params
     ) public returns (bool) {
         // Task 2.1 - Check that msg.sender is the pool contract
+        require(msg.sender == address(pool), "caller invalid");
 
         // Task 2.2 - Check that initiator is this contract
+        require(initiator == address(this), "initiator invalid");
 
         // Task 2.3 - Decode caller from params and transfer
         // flash loan fee from this caller
+        address owner = abi.decode(params, (address));
+        IERC20(asset).transferFrom(owner, address(this), fee);
 
         // Task 2.4 - Approve the pool to spend flash loaned amount + fee
+        IERC20(asset).approve(address(pool), amount + fee);
 
         // Task 2.5 - Return true
+        return true;
     }
 }
