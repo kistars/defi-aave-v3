8fda5c55c66e62c6613409400deac1b1ecf04f30
kistars <sunllen2014@outlook.com>
2025-08-03 11:46:43 +0800

    feat: long short exercise

--- foundry/src/exercises/LongShort.sol
+++ foundry/src/exercises/LongShort.sol
@@ -31,22 +31,45 @@ contract LongShort is Aave, Swap {
         returns (uint256 collateralAmountOut)
     {
         // Task 1.1 - Check that params.minHealthFactor is greater than 1e18
+        require(params.minHealthFactor > 1e18, "minHealthFactor invalid");
 
         // Task 1.2 - Transfer collateral from msg.sender
+        IERC20(params.collateralToken).transferFrom(
+            msg.sender, address(this), params.collateralAmount
+        );
 
         // Task 1.3
         // - Approve and supply collateral to Aave
         // - Send aToken to msg.sender
+        supply(params.collateralToken, params.collateralAmount, address(this));
+        IERC20(getATokenAddress(params.collateralToken)).transfer(
+            msg.sender, getATokenBalance(params.collateralToken, address(this))
+        );
 
         // Task 1.4
         // - Borrow token from Aave
         // - Borrow on behalf of msg.sender
+        borrow(params.borrowToken, params.borrowAmount, msg.sender);
 
         // Task 1.5 - Check that health factor of msg.sender is > params.minHealthFactor
+        require(
+            getHealthFactor(msg.sender) > params.minHealthFactor,
+            "health factor too low"
+        );
 
         // Task 1.6
         // - Swap borrowed token to collateral token
         // - Send swapped token to msg.sender
+        uint256 amountOut = swap(
+            params.borrowToken,
+            params.collateralToken,
+            params.borrowAmount,
+            params.minSwapAmountOut,
+            address(this),
+            params.swapData
+        );
+        IERC20(params.collateralToken).transfer(msg.sender, amountOut);
+        return amountOut;
     }
 
     struct CloseParams {
@@ -73,20 +96,42 @@ contract LongShort is Aave, Swap {
         )
     {
         // Task 2.1 - Transfer collateral from msg.sender into this contract
-
+        IERC20(params.collateralToken).transferFrom(
+            msg.sender, address(this), params.collateralAmount
+        );
         // Task 2.2 - Swap collateral to borrowed token
+        uint256 swapAmount = swap(
+            params.collateralToken,
+            params.borrowToken,
+            params.collateralAmount,
+            params.minSwapAmountOut,
+            address(this),
+            params.swapData
+        );
 
         // Task 2.3
         // - Repay borrowed token
         // - Amount to repay is the minimum of current debt and params.maxDebtToRepay
         // - If the amount to repay is greater that the amount swapped,
         //   then transfer the difference from msg.sender
+        uint256 debt = getVariableDebt(params.borrowToken, address(this));
+        uint256 repay = Math.min(debt, params.maxDebtToRepay);
+        if (repay > swapAmount) {
+            IERC20(params.borrowToken).transferFrom(
+                msg.sender, address(this), repay - swapAmount
+            );
+        }
 
         // Task 2.4 - Withdraw collateral to msg.sender
+        uint256 withdrawn = withdraw(
+            params.collateralToken, params.collateralAmount, msg.sender
+        );
 
         // Task 2.5 - Transfer profit = swapped amount - repaid amount
+        uint256 profit = swapAmount - repay;
 
         // Task 2.6 - Return amount of collateral withdrawn,
         //            debt repaid and profit from closing this position
+        return (withdrawn, repay, profit);
     }
 }
