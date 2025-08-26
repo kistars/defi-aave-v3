e2562b29bdf9ab28def9fe43c66421b1018aa9f0
kistars <sunllen2014@outlook.com>
2025-08-26 18:37:49 +0800

    feat:liquidate exercise

--- foundry/src/exercises/Liquidate.sol
+++ foundry/src/exercises/Liquidate.sol
@@ -14,11 +14,23 @@ contract Liquidate {
         public
     {
         // Task 1.1 - Get the amount of borrowed token that the user owes to Aave V3
+        address debtToken =
+            pool.getReserveData(borrowedToken).variableDebtTokenAddress;
+        uint256 amount = IERC20(debtToken).balanceOf(user);
 
         // Task 1.2 - Transfer the full borrowed amount from msg.sender
+        IERC20(borrowedToken).transferFrom(msg.sender, address(this), amount);
 
         // Task 1.3 - Approve the pool contract to spend borrowed token from this contract
+        IERC20(borrowedToken).approve(address(pool), amount);
 
         // Task 1.4 - Call liquidate
+        pool.liquidationCall({
+            collateralAsset: collateral,
+            debtAsset: borrowedToken,
+            user: user,
+            debtToCover: amount,
+            receiveAToken: false
+        });
     }
 }
